97b0105ae77c8a2460ae3e3f6e90840614982996
André Lamego Reis de Castro <andrelamegobh@hotmail.com>
2022-05-03 18:41:09 -0300

    Auto stash before revert of "Final login with new buttons"

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,6 +7,7 @@ function Login()
     return(
         <div className="base">
             <div className="container">
+<<<<<<< Updated upstream
                 <div>
                     <img src="/images/Quick_Notes.png" alt="Quick Notes"/>
                     <Form className="inputs">
@@ -22,6 +23,29 @@ function Login()
                     </Form>
                 </div>
                 
+=======
+                <img className="mb-3" class name ="image" src="/images/Quick_Notes.png" alt="Quick Notes"/>
+                <Form className="inputs">
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Control 
+                            type="email" 
+                            placeholder="E-mail" 
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="senha">
+                        <Form.Control 
+                            type="password" 
+                            placeholder="Senha" 
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" onClick={login}>
+                        Login
+                    </Button>
+                </Form>
+>>>>>>> Stashed changes
             </div>
         </div> 
     );
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,23 @@ import { BrowserRouter , Route , Switch } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Menu from "./pages/Menu";
 
 function Routes()
 {
     return(
         <BrowserRouter>
             <Switch>
+<<<<<<< Updated upstream
                 <Route path="/login" component={Login}/>
                 <Route path="/cadastro" component={Cadastro}/>
                 <Route path="/" component={Home}/>
+=======
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/cadastro" component={Cadastro}/>
+                <Route exact path="/home" component={Home}/>
+                <Route exact path="/temp" component={Menu}/>
+>>>>>>> Stashed changes
             </Switch>
         </BrowserRouter>
     );
